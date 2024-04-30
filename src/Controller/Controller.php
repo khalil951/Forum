c658825356aae6303861e50d3fa2ac7bf2e2048d
khalil951 <mohamedkhalil.ouelsleti@esprit.tn>
2024-04-30 22:24:22 +0100

    added dynamic search feature

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Room;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
